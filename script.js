8f932d490c46ca9a0e615cac1d7165a1d3dc18d8
Swag-blips <coderblip@gmail.com>
2024-06-24 10:39:07 +0100

    first challenge solved

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-"use strict"
+"use strict";
 
-// function that takes string as an input
+// Write a JavaScript function that takes a string as an input and returns the string with all vowels removed.
+
+const vowelReplace = (str) => {
+  return str
+    .toLowerCase()
+    .split("")
+    .filter((char) => !"aeiou".includes(char))
+    .join("");
+};
+
+const measurePerformance = () => {
+  const testString =
+    "This is a performance test string with a significant length to measure the time taken to remove vowels.";
+
+  let startTime = performance.now();
+
+  const result = vowelReplace(testString);
+
+  let endTime = performance.now();
+
+  const duration = endTime - startTime;
+
+  console.log(`output: ${result}`);
+  console.log(`duration: ${duration}`);
+};
+
+measurePerformance();
