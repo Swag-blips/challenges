27b83c4ff9d0e34786e8289981f052a07fdc9ed3
Swag-blips <coderblip@gmail.com>
2024-06-24 15:20:46 +0100

    more functions added

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const measurePerformance = () => {
 
 measurePerformance();
 
-// Reverse a String: Write a function that takes a string as input and returns the reversed string.
+//1. Reverse a String: Write a function that takes a string as input and returns the reversed string.
 
 const reverseStr = (arg) => {
   if (typeof arg !== "string")
@@ -40,7 +40,7 @@ const reverseStr = (arg) => {
 
 console.log(reverseStr("hello"));
 
-// 1. Find the Maximum: Write a function that takes an array of numbers as input and returns the maximum value.
+// 2. Find the Maximum: Write a function that takes an array of numbers as input and returns the maximum value.
 
 const maximumNum = (arg) => {
   if (!Array.isArray(arg))
@@ -57,18 +57,9 @@ const maximumNum = (arg) => {
 
   return maxNum;
 };
-
 console.log(maximumNum([1, 2, 3, 5, 6, 8, 9, 10, 20, 59, 80]));
 
-//Sort an Array: Write a function that takes an array of numbers as input and returns the sorted array in ascending order.
-
-const sortArray = (arg) => {
-  return arg.sort((a, b) => a - b);
-};
-
-console.log(sortArray([1, 3, 7, 4, 8, 6, 9, 2, 5, 10]));
-
-//Check if Palindrome: Write a function that takes a string as input and returns true if it's a palindrome, false otherwise.
+//3. Check if Palindrome: Write a function that takes a string as input and returns true if it's a palindrome, false otherwise.
 
 const isPalindrome = (arg) => {
   if (typeof arg !== "string")
@@ -78,13 +69,34 @@ const isPalindrome = (arg) => {
   let reversedStr = arg.toLowerCase().split("").reverse().join("");
   return reversedStr === arg.toLowerCase();
 };
-
 console.log(isPalindrome("kayak"));
 
-// // function to create fiboacci sequence
-// const fibonacciSequence = () => {
+//4. Fibonacci Sequence: Write a function that takes a number as input and returns the corresponding Fibonacci number.
 
-// }
+const fibonacciRecursive = (n) => {
+  // dont fully understand this code
+
+  if (typeof n !== "number" || n < 0) {
+    throw new Error("Input must be a non-negative integer");
+  }
+
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
+
+console.log(fibonacciRecursive(5));
+
+//5. Sort an Array: Write a function that takes an array of numbers as input and returns the sorted array in ascending order.
+
+const sortArray = (arg) => {
+  return arg.sort((a, b) => a - b);
+};
+
+console.log(sortArray([1, 3, 7, 4, 8, 6, 9, 2, 5, 10]));
+
+//6. Find the First Duplicate: Write a function that takes an array of numbers as input and returns the first duplicate value.
 
 const firstDuplicate = (arg) => {
   if (!Array.isArray(arg)) throw new Error("this function takes only an arra");
@@ -119,8 +131,12 @@ const firstDuplicateAlt = (arg) => {
 
 console.log(firstDuplicateAlt([1, 2, 3, 4, 4, 5, 5]));
 
-//Count the Vowels: Write a function that takes a string as input and returns the number of vowels.
+//7. Count the Vowels: Write a function that takes a string as input and returns the number of vowels.
 const vowelCount = (arg) => {
+  if (typeof arg !== "string")
+    throw new Error(
+      "this function does not accept any parameter except strings"
+    );
   let count = 0;
 
   for (let i = 0; i < arg.length; i++) {
@@ -128,8 +144,57 @@ const vowelCount = (arg) => {
       count++;
     }
   }
-
   return count;
 };
 
-console.log(vowelCount("typeshit"));
+console.log(vowelCount("helloo"));
+// 8 Check if Array is Sorted: Write a function that takes an array of numbers as input and returns true if it's sorted in ascending order, false otherwise.
+
+const isSorted = (arg) => {
+  if (!Array.isArray(arg)) throw new Error("this function takes only an arra");
+
+  let initArg = [];
+  initArg = [...arg];
+
+  const sorted = arg.sort((a, b) => a - b);
+  console.log(initArg);
+
+  console.log(sorted);
+
+  return initArg.toString() === sorted.toString();
+};
+
+console.log(isSorted([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+//9 Find the Longest Word: Write a function that takes a string as input and returns the longest word.
+
+const longestWord = (arg) => {
+  if (typeof arg !== "string")
+    throw new Error(
+      "this function does not accept any parameter except strings"
+    );
+
+  if (arg.trim() === "") return;
+  const wordSplit = arg.toLowerCase().split(" ");
+  let initialWord = wordSplit[0];
+
+  for (let i = 0; i < wordSplit.length; i++) {
+    if (wordSplit[i].length > initialWord.length) {
+      initialWord = wordSplit[i];
+    }
+  }
+  return initialWord;
+};
+
+console.log(longestWord("i love javascript"));
+
+//10 Calculate the Sum: Write a function that takes an array of numbers as input and returns the sum of all numbers.
+
+const sumOfArray = (arg) => {
+  const sum = arg.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+
+  return sum;
+};
+
+console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 2]));
